f8ee4385b1058269288ee474dd6c069e11de442e
Patrick Thiel <patrickthiel@outlook.de>
2023-06-03 11:10:48 +0200

    Experimented with click-on picture

--- index.js
+++ index.js
@@ -109,3 +109,7 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+$('DIV#images img').click(function () {
+  $('DIV #text').html($(this).data('text')));
+});
