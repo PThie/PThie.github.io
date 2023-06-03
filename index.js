d879fc684a9ca03af364f3c562e20d2991a13a3e
Patrick Thiel <patrickthiel@outlook.de>
2023-06-03 12:22:38 +0200

    Experimented with click-on picture

--- index.js
+++ index.js
@@ -109,24 +109,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-
-var textOverImages = document.getElementsByClassName("onClickTextOverImage");
-var previousTextOverImage;
-
-for (var i = 0; i < textOverImages.length; i++) {
-  textOverImages[i].onclick = function() {
-    var classes = this.classList;
-    if (classes.contains("show")) {
-      classes.remove("show");
-    } else {
-      if (previousTextOverImage != null)
-      previousTextOverImage.classList.remove("show");
-      previousTextOverImage = this;
-      classes.add("show");
-    }
-  }
-}
-
-function stopPropagation(event){
-  event.stopPropagation();
-}
